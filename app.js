08fe634e23f7bf543ab404cfd50f80cd7df637a4
subaruuuu <meteorbs13@gmail.com>
2020-11-18 17:01:16 +0800

    feat: finish detail function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 // const restaurantList = require('./restaurant.json')  // 引進種子資料所以註銷掉
 const mongoose = require('mongoose')          // 引進 mongoose
-const Restaurant = require('./models/rest')   // 引進 rest model
+const Restaurant = require('./models/rest')   // 引進 rest model 建構子
 
 // mongodb setting
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,6 +27,8 @@ app.use(express.static('public'))
 
 // router setting
 app.get('/', (req, res) => {
+  // res.render('index', { restaurant: restaurantList.results })
+
   // get all data from "Restaurant"
   Restaurant.find()
     .lean()
@@ -34,19 +36,17 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// app.get('/', (req, res) => {
-//   res.render('index', { restaurant: restaurantList.results })
-// })
-
 // check detail
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.restaurant_id)
-  res.render('show', { restaurantDetail: restaurant[0] })
+  // const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.restaurant_id)
+  // res.render('show', { restaurantDetail: restaurant[0] })
 
-  // Restaurant.findById(req.params.restaurant_id)   //從這裡開始繼續修改
-  //   .lean()
-  //   .then(restaurant => res.render('show', { restaurant: restaurant }))
-  //   .catch(error => console.log(error))
+
+  const id = req.params.restaurant_id
+  Restaurant.findOne({ id: id })   //從這裡開始繼續修改
+    .lean()
+    .then(restaurant => res.render('show', { restaurantDetail: restaurant }))
+    .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
