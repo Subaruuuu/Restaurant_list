953e63df0d36899c0c90740eddf242b460b0ee63
subaruuuu <meteorbs13@gmail.com>
2020-11-18 16:12:34 +0800

    feat: add edit, detail, delete button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+// const restaurantList = require('./restaurant.json')  // 引進種子資料所以註銷掉
 const mongoose = require('mongoose')          // 引進 mongoose
 const Restaurant = require('./models/rest')   // 引進 rest model
 
@@ -27,12 +27,26 @@ app.use(express.static('public'))
 
 // router setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurantList.results })
+  // get all data from "Restaurant"
+  Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', { restaurant: restaurant }))
+    .catch(error => console.log(error))
 })
 
+// app.get('/', (req, res) => {
+//   res.render('index', { restaurant: restaurantList.results })
+// })
+
+// check detail
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.filter(restaurant => restaurant.id == req.params.restaurant_id)
   res.render('show', { restaurantDetail: restaurant[0] })
+
+  // Restaurant.findById(req.params.restaurant_id)   //從這裡開始繼續修改
+  //   .lean()
+  //   .then(restaurant => res.render('show', { restaurant: restaurant }))
+  //   .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
