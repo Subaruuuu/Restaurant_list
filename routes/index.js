1081c566863db835502954b3faeb2f0ace8a53ed
subaruuuu <meteorbs13@gmail.com>
2021-01-13 11:13:30 +0800

    feat: add user module and login page

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
 router.use('/restaurants', restaurant)
 router.use('/', home)
 router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router
