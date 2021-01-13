eed89437689f2802c376432cae58fd5f6d93d6ee
subaruuuu <meteorbs13@gmail.com>
2021-01-13 14:31:27 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes/index')
+const session = require('express-session')
 require('./config/mongoose')
 
+app.use(session({
+  secret: 'restSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
